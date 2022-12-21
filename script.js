1fa31b2159939fb46fdc666d73ef02b6699876bd
tioburla <gabrielrochagbl@gmail.com>
2022-12-21 20:55:57 -0300

    Update Files

--- script.js
+++ script.js
@@ -6,10 +6,15 @@ function digitando() {
     if(inserir.value != "") {
         qr.style.opacity = 0.9;
         divqr.style.opacity = 0.9;
-        return qr.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inserir.value}`;
+        qr.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inserir.value}`;
+    
     }else{
         qr.style.opacity = 0;
         divqr.style.opacity = 0;
     }
-};    
+}; 
+
+
+
+
 
